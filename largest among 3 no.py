b4c12c5a6194e126978108b771a29c1b0f01b503
Nandana-prog <nandanarpkumar@gmail.com>
2026-01-21 16:39:46 +0530

    Add files via upload

---
+++ largest among 3 no.py
@@ -0,0 +1,10 @@
+a=int(input("ënter a number"))
+b=int(input ("enter second number"))
+c=int(input("ënter third number"))
+
+if a>b and a>c :
+    print("ä is the largest")
+elif b>a and b>c :
+    print("b is the largest")   
+
+print("c is largest")   
