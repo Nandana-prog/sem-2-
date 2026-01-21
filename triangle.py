baffbf342198ea40368c16be95b8ce72fc2427be
Nandana-prog <nandanarpkumar@gmail.com>
2026-01-21 17:04:27 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,8 @@
+a=int(input("ënter angle 1"))
+b=int(input("ënter angle 2"))
+c=int(input("ënter angle 3"))
+
+if a+b+c==180:
+    print("a valid triangle")
+else:
+    print("not a valid triangle")
