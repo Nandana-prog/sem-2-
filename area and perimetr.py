60f378ff8fd42f2fc4868e6d1cc6b89385ed421c
Nandana-prog <nandanarpkumar@gmail.com>
2026-01-21 17:25:12 +0530

    Add files via upload

---
+++ area and perimetr.py
@@ -0,0 +1,10 @@
+a=int(input("ënter length"))
+b=int(input("enter breadth"))
+
+area=a*b
+perimeter=2*(a+b)
+if area>perimeter:
+    print("area is greater")
+
+else:
+    print("perimetr is greater")
---
+++ colinear.py
@@ -0,0 +1,12 @@
+x1=int(input("ënter x1"))
+y1=int(input("enter y1"))
+x2=int(input("enter x2"))
+y2=int(input("enter y2"))
+x3=int(input("enter x3"))
+y3=int(input("enter y3"))
+
+if (x2-x1)*(y3-y1)==(y2-y1)*(x3-x1):
+    print("line is colinear")
+
+else:
+    print("not colinear")
